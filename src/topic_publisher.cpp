27faf2bde99e730ddda22001cec58b0d866a2500
is0232xf <is0232xf@gmail.com>
2019-03-15 23:11:31 +0900

    modify some files not to be occur catkin_make errors

--- src/topic_publisher.cpp
+++ src/topic_publisher.cpp
@@ -1,14 +1,14 @@
 #include "ros/ros.h"
-#include "ros_tutorials_topic/MsgTutrial.h"
+#include "ros_tutorials_topic/MsgTutorial.h"
 
 int main(int argc, char **argv)
 {
-	ros::init(argc,argv, "topic_publisher");
+	ros::init(argc, argv, "topic_publisher");
 	ros::NodeHandle nh;
 
 	ros::Publisher ros_tutorial_pub =
 		nh.advertise<ros_tutorials_topic::MsgTutorial>
-		("ros_tutorial_msg:, 100");
+		("ros_tutorial_msg:", 100);
 	ros::Rate loop_rate(10);
 	ros_tutorials_topic::MsgTutorial msg;
 	int count = 0;
